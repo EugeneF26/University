5e3f1e30029d3266bac268f5cc49659827968ac1
Eugene <evgeniykadyshef@gmail.com>
2020-01-31 13:28:04 +0300

    create javadoc for Student

--- src/main/java/com/project/university/entity/Student.java
+++ src/main/java/com/project/university/entity/Student.java
@@ -1,47 +1,91 @@
 package com.project.university.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The class contain data of student and methods working with it
+ */
 public class Student {
-	
+
 	private String name;
 	private String surname;
+	private int id;
+	private List<Student> students;
+	private Group group;
 
+	/**
+	 * The constructor for filling data
+	 * @param name of student string value
+	 * @param surname of student string value
+	 */
 	public Student(String name, String surname) {
 		this.setName(name);
 		this.setSurname(surname);
 	}
-	
-	public List<Student> getStudents(){
-		return null;
-	}
-	
-	public void expelStudent(Student student) {
-		
+
+	/**
+	 * The method return list of students by group
+	 * @param titleGroup of group string value
+	 * @return list of student by group
+	 */
+	public List<Student> getStudents(String titleGroup) {
+		List<Student> studentsByGroup = new ArrayList<>();
+		for (int i = 0; i < students.size(); i++) {
+			if (students.get(i).group.getGroup().equals(titleGroup)) {
+				studentsByGroup.add(students.get(i));
+			}
+		}
+		return studentsByGroup;
 	}
 
+	/**
+	 * The method delete student from list of students
+	 * @param student instance of Student
+	 * @see Student#Student(String, String)
+	 */
+	public void expelStudent(Student student) {
+		for (int i = 0; i < students.size(); i++) {
+			if (students.get(i).getId() == student.getId()) {
+				students.remove(i);
+			}
+		}
+	}
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getSurname() {
 		return surname;
 	}
 
-
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
 
-
 	public void setStudents(List<Student> students) {
+
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
 
