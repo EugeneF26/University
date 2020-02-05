77bab2b5c91433d00d9b8bbf3bc6dbf9ddb330cc
Eugene <evgeniykadyshef@gmail.com>
2020-02-05 14:47:28 +0300

    wrote javadoc

--- src/main/java/com/project/university/dao/CrudRepository.java
+++ src/main/java/com/project/university/dao/CrudRepository.java
@@ -2,11 +2,39 @@ package com.project.university.dao;
 
 import java.util.List;
 
+/** @author Eugene
+ * Interface for generic CRUD operations on a repository for a specific type.
+ * @param <T>
+ */
 public interface CrudRepository<T> {
+	
+	/** Saves a given entity
+	 * @param t - must not be null.
+	 * @return the count saved line
+	 */
 	int save(T t);
+	
+	/** Retrieves an entity by its id.
+	 * @param id - must not be null
+	 * @return the given entity
+	 */
 	T find(int id);
+	
+	/** Update a given entity
+	 * @param t - must not be null
+	 * @return the count update line
+	 */
 	int update(T t);
+	
+	/** Deletes by given id. 
+	 * @param id - must not be null
+	 * @return the count delete line
+	 */
 	int delete(int id);
+	
+	/** Returns all instances of the type.
+	 * @return all entities
+	 */
 	List<T> getAll();
 }
 
