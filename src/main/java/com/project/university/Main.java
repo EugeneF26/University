14d58cdf48210fe0255ec63f763357477c9e7ff1
Eugene <evgeniykadyshef@gmail.com>
2020-02-05 14:47:15 +0300

    delete javadoc

--- src/main/java/com/project/university/Main.java
+++ src/main/java/com/project/university/Main.java
@@ -1,14 +1,6 @@
 package com.project.university;
 
-/** @author Eugene
- * <strong>The main class in application</strong> 
- */
 public class Main {
-	/**
-	 * Here start point of the program
-	 * @param args command line values
-	 * @see UniversityApplication#run() 
-	 */
 	public static void main(String[] args) {
 		UniversityApplication universityApplication = new UniversityApplication();
 		universityApplication.run();
