b04bc5cbd876039e804073da0ebeac15b31103dc
Eugene <evgeniykadyshef@gmail.com>
2020-02-05 23:06:15 +0300

    create spring config

---
+++ src/main/java/com/project/university/SpringConfig.java
@@ -0,0 +1,26 @@
+package com.project.university;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.project.university.dao")
+public class SpringConfig {
+	
+	@Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("${db.DRIVER]");
+        dataSource.setUrl("${db.HOST}");
+        dataSource.setUsername("${db.USERNAME}");
+        dataSource.setPassword("${db.PASSWORD}");
+        return dataSource;
+    }
+
+}
