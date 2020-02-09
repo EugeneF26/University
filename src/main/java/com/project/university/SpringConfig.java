f82e0b5cd0f1ff4435c956ad29409413c0e1a63e
Eugene <evgeniykadyshef@gmail.com>
2020-02-09 07:01:58 +0300

    added namedParameterJdbcTemplate

--- src/main/java/com/project/university/SpringConfig.java
+++ src/main/java/com/project/university/SpringConfig.java
@@ -1,11 +1,15 @@
 package com.project.university;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 /** @author Eugene
  * A factory for connections to the physical data source that this DataSource object represents
@@ -15,18 +19,29 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @ComponentScan("com.project.university.dao")
 public class SpringConfig {
 	
+	@Value("${db.DRIVER]") private String DRIVER;
+	@Value("${db.HOST}") private String HOST;
+    @Value("${db.USERNAME}") private String USER_NAME;
+	@Value("${db.PASSWORD}") private String PASSWORD;
+	
 	/**
 	 * Attempts to establish a connection with the data source
 	 * @return jdbcTemplate
 	 */
 	@Bean
     public JdbcTemplate dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("${db.DRIVER]");
-        dataSource.setUrl("${db.HOST}");
-        dataSource.setUsername("${db.USERNAME}");
-        dataSource.setPassword("${db.PASSWORD}");
-        return new JdbcTemplate(dataSource);
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DRIVER);
+        hikariDataSource.setJdbcUrl(HOST);
+        hikariDataSource.setUsername(USER_NAME);
+        hikariDataSource.setPassword(PASSWORD);
+        return new JdbcTemplate(hikariDataSource);
     }
+	
+	@Bean
+	public NamedParameterJdbcTemplate nameParamJdbcTemplate() {
+		NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource());
+		return namedParameterJdbcTemplate;
+	}
 }
 
