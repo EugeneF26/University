8478aded1825476993286920ae58faf1c68b7367
Eugene Kadyshev <evgeniykadyshef@gmail.com>
2020-02-21 16:15:52 +0300

    update tests

--- src/main/java/com/project/university/repository/GroupRepository.java
+++ src/main/java/com/project/university/repository/GroupRepository.java
@@ -1,5 +1,6 @@
 package com.project.university.repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class GroupRepository implements CrudRepository<Group> {
 	 * @see CrudRepository#save(Object)
 	 */
 	@Override
-	public Group save(Group group) {
+	public Group save(Group group) {	
 		this.jdbcTemplate.update("INSERT INTO GROUPS (courseId) VALUES (?)", group.getCourseId().getId());
 		return this.jdbcTemplate.queryForObject("SELECT id FROM GROUPS WHERE courseId=?", 
 				BeanPropertyRowMapper.newInstance(Group.class) , group.getCourseId().getId());
@@ -63,8 +64,9 @@ public class GroupRepository implements CrudRepository<Group> {
 	 * @see CrudRepository#uddate(Object)
 	 */
 	@Override
-	public Group update(Group group) {
-		this.jdbcTemplate.update("UPDATE GROUPS SET courseId=? WHERE id=? ", group.getCourseId().getId(), group.getId());
+	public Group update(Group group) {	
+		this.jdbcTemplate.update("UPDATE COURSES_GROUPS SET groupId=? WHERE courseId=?", group.getId(), 
+				group.getCourseId().getId());
 		return group;
 	}
 
