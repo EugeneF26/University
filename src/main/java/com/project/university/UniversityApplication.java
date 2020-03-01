93f55727b1924e28cb7d001dca8be1be5b5f4117
Eugene Kadyshev <evgeniykadyshef@gmail.com>
2020-03-01 21:49:24 +0300

    deleted war

--- src/main/java/com/project/university/UniversityApplication.java
+++ src/main/java/com/project/university/UniversityApplication.java
@@ -1,13 +1,31 @@
 package com.project.university;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.project.university.controller.UniversityController;
 import com.project.university.model.Professor;
 import com.project.university.model.Student;
 
 /**
  * @author Eugene The class contain methods for managing data of other classes
  */
+@Configuration
+@ComponentScan
 public class UniversityApplication {
-
+	
+	private UniversityController universityController;
+	
+	@Autowired
+	public UniversityApplication(UniversityController universityController) {
+		this.universityController = universityController;
+	}
+	
+	public UniversityApplication() {
+		
+	}
+	
 	/*
 	 * The method start methods of application
 	 */
@@ -27,3 +45,4 @@ public class UniversityApplication {
 	private void acceptNewStudentToCourse(Student student) {
 	}
 }
+
