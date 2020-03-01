b84cb6c9b566ff3ddc98c96bd5e9adadf6eaae9c
Eugene Kadyshev <evgeniykadyshef@gmail.com>
2020-03-01 20:54:07 +0300

    update html files and deleted some methods of controller

--- src/main/java/com/project/university/config/DatasourseConfiguration.java
+++ src/main/java/com/project/university/config/DatasourseConfiguration.java
@@ -2,6 +2,7 @@ package com.project.university.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/com/project/university/controller/UniversityController.java
+++ src/main/java/com/project/university/controller/UniversityController.java
@@ -1,5 +1,7 @@
 package com.project.university.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UniversityController {
 
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index(Model model) {
 		model.addAttribute("message", "Hello Spring MVC 5!");
 		return "index";
+	}
+	
+	@GetMapping("/serverTime")
+	public String date(Model model) {
+		model.addAttribute("time", new Date());
+		return "serverTime";
 	}
 }
 
