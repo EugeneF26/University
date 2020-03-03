62933bb3f892789fcf0b507b69cf28a6d69b7fca
Eugene Kadyshev <evgeniykadyshef@gmail.com>
2020-03-04 00:30:09 +0300

    create new html files and a new method for controller

--- src/main/java/com/project/university/controller/UniversityController.java
+++ src/main/java/com/project/university/controller/UniversityController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UniversityController {
 
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index(Model model) {
-		model.addAttribute("message", "Hello Spring MVC 5!");
 		return "index";
 	}
 	
+	@GetMapping("/list")
+	public String accept(Model model) {
+		return "list";
+	}
 }
 
