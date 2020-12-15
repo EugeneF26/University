630d6e2f120db63e62512a90d585e9cdd75b83ba
Eugene <evgeniykadyshef@gmail.com>
2020-12-15 17:28:38 +0300

    finish back chapter my project

--- src/main/java/com/project/university/controller/LectureController.java
+++ src/main/java/com/project/university/controller/LectureController.java
@@ -1,10 +1,13 @@
 package com.project.university.controller;
 
+import com.project.university.model.Lecture;
 import com.project.university.service.LectureService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LectureController {
@@ -24,6 +27,19 @@ public class LectureController {
     @GetMapping("/lectures/delete/{id}")
     public String delete(@PathVariable("id") long id) throws Exception {
         lectureService.deleteLecture(id);
+        return "redirect:/lectures/list";
+    }
+
+    @GetMapping("/lectures/save")
+    public String save(Model model) throws Exception {
+        Lecture lecture = new Lecture();
+        model.addAttribute("lecture", lecture);
+        return "lectures_table/save";
+    }
+
+    @PostMapping("/lectures/save/form")
+    public String createSaveForm(@ModelAttribute("lecture") Lecture lecture) {
+        lectureService.addNewLecture(lecture);
         return "redirect:/lectures/list";
     }
 }
--- src/main/java/com/project/university/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/project/university/service/impl/ScheduleServiceImpl.java
@@ -1,9 +1,5 @@
 package com.project.university.service.impl;
 
-import java.sql.Date;
-
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.project.university.service.ScheduleService;
