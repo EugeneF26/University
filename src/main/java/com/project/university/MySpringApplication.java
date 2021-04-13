d8a732adf91708d4aae505b30eb86bf53223a5f8
Eugene <evgeniykadyshef@gmail.com>
2021-04-13 16:17:18 +0300

    added a link to css

--- src/main/java/com/project/university/MySpringApplication.java
+++ src/main/java/com/project/university/MySpringApplication.java
@@ -1,7 +1,6 @@
 package com.project.university;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
