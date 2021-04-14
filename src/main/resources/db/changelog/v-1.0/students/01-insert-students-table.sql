0e273c7c6ecb62c46daf92928acc75fada360fe7
Eugene <evgeniykadyshef@gmail.com>
2021-04-14 08:30:13 +0300

    created a new card

--- src/main/resources/db/changelog/v-1.0/students/01-insert-students-table.sql
+++ src/main/resources/db/changelog/v-1.0/students/01-insert-students-table.sql
@@ -1,4 +1,8 @@
 INSERT STUDENTS(name, surname, groupId, currentStatus)
-VALUES ('Stepan', 'Pushkin', 1, 'STUDY');
+VALUES ('Stepan', 'Pushkin', 1, 'STUDY'),
+       ('Fedor', 'Lakov', 1, 'STUDY'),
+       ('Alex', 'Hamatov', 1, 'STUDY'),
+       ('Anton', 'Pronin', 1, 'STUDY'),
+       ('Anna', 'Filippova', 1, 'STUDY');
 
 GO
